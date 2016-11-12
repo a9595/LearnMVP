f2f16f5e569e64a115234192643175145525aa61
Andrii Kovalchuk <tieorange@gmail.com>
2016-11-12 21:43:50 +0100

    started recycler

--- app/src/main/java/edu/tieorange/learnmvp/ui/RepoAdapter.java
+++ app/src/main/java/edu/tieorange/learnmvp/ui/RepoAdapter.java
@@ -1,5 +1,6 @@
 package edu.tieorange.learnmvp.ui;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
@@ -7,22 +8,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.databinding.library.baseAdapters.BR;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.tieorange.learnmvp.R;
+import edu.tieorange.learnmvp.User;
 
 /**
  * Created by root on 11/12/16.
  */
-public class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.ViewHolder>{
+public class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.ViewHolder> {
+    private Context mContext;
+    private List<User> mMyModels = new ArrayList<>();
+
+    public RepoAdapter(Context context, List<User> myModels) {
+        mContext = context;
+        mMyModels = myModels;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        ViewDataBinding viewDataBinding = DataBindingUtil.inflate(layoutInflater, R.layout.item_repo, parent, false);
-        return null;
+//        ViewDataBinding viewDataBinding = DataBindingUtil.inflate(layoutInflater, R.layout.item_repo, parent, false);
+        View view = layoutInflater.inflate(R.layout.item_repo, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        User user = mMyModels.get(position);
 
+        ViewDataBinding viewDataBinding = holder.mViewDataBinding;
+        viewDataBinding.setVariable(BR.user, user);
     }
 
     @Override
@@ -30,9 +49,12 @@ public class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.ViewHolder>{
         return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        public ViewHolder(View itemView) {
-            super(itemView);
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ViewDataBinding mViewDataBinding;
+
+        public ViewHolder(View view) {
+            super(view);
+            DataBindingUtil.bind(view);
         }
     }
 }
