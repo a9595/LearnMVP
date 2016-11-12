c0e84cd3376bea05e45aa02d603b846117961a15
Andrii Kovalchuk <tieorange@gmail.com>
2016-11-12 19:24:48 +0100

    2 way binding

--- app/src/main/java/edu/tieorange/learnmvp/User.java
+++ app/src/main/java/edu/tieorange/learnmvp/User.java
@@ -1,16 +1,35 @@
 package edu.tieorange.learnmvp;
 
+import android.databinding.BindingAdapter;
+import android.databinding.InverseBindingAdapter;
 import android.databinding.ObservableField;
+import android.databinding.ObservableInt;
+import android.support.v4.util.Pair;
+import android.support.v7.widget.AppCompatEditText;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by root on 11/12/16.
  */
 public class User {
-    public ObservableField<String> username;
-    public ObservableField<String> password;
+    public ObservableField<String> username = new ObservableField<>();
+    public ObservableField<String> password = new ObservableField<>();
+
+    public User() {
+        this("", "");
+    }
 
     public User(String username, String password) {
         this.username.set(username);
         this.password.set(password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username=" + username.get() +
+                ", password=" + password.get() +
+                '}';
     }
 }
